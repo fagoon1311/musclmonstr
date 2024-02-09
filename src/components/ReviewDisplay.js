cac0807d6871785ae851812193286451260bd867
fagoon1311 <fs14smailbox65@gmail.com>
2024-02-10 00:20:36 +0530

    .

--- src/components/ReviewDisplay.js
+++ src/components/ReviewDisplay.js
@@ -9,7 +9,7 @@ const ReviewDisplay = () => {
       <h1 className='text-3xl text-[#BF6900]'>What our Clients say</h1>
       <div className='border-b-4 border-[#BF6900] h-3 w-52'></div>
     </div>
-  <div className='flex flex-row gap-10 overflow-x-auto py-7'>
+  <div className='flex flex-row gap-10 overflow-x-auto py-7'style={{ '-ms-overflow-style': 'none', 'scrollbar-width': 'none' }} >
     {
         positiveReviewsArray.map((item, index)=>(<ReviewCard key={index} info={item}/>))
     }
